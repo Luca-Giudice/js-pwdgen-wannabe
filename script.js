104f51e2283381c9d341a0a9de780328ac24b0c8
Luca-Giudice <2giudice4@gmail.com>
2022-12-02 17:06:05 +0100

    exercise

---
+++ script.js
@@ -0,0 +1,16 @@
+// Chiedere al'utente nome cognome colore preferito e generare una password con alla fine il numero 21
+
+
+
+// 1. Prendo l'elemento DOM
+// 2. Chiedo il nome all'utente
+// 3. Chiedo il cognome all'utente
+// 4. Chiedo il colore preferito all'utente
+// 5. Calcolare il prodotto del nome più l cognome più il colore preferito aggiungendo 21
+// 6. Inserirlo nell'HTML
+
+
+// Prendo l'elemento DOM
+
+const targetElement = document.getElementById('targent');
+console.log(targetElement);
