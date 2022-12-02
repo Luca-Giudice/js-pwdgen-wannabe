00cb1c6b19f305542f05d2163c247b4cb4dbb8dc
Luca-Giudice <2giudice4@gmail.com>
2022-12-02 18:30:57 +0100

    finish

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Chiedere al'utente nome cognome colore preferito e generare una password con alla fine il numero 21
+// | Chiedere al'utente nome cognome colore preferito e generare una password con alla fine il numero 21
 
 
 
@@ -7,7 +7,7 @@
 // 3. Chiedo il cognome all'utente
 // 4. Chiedo il colore preferito all'utente
 // 5. Calcolare il prodotto del nome più l cognome più il colore preferito aggiungendo 21
-// 6. Inserirlo nell'HTML
+// 6. Inserire  il risultato nell'elemento DOM
 
 
 // Prendo l'elemento DOM
@@ -18,5 +18,26 @@ console.log(targetElement);
 
 // chiedo il nome all'utente
 
-const unerName = parseInt('Come ti chiami?');
-console.log()
+const userName = prompt('Nome');
+console.log(userName);
+
+
+// Chiedo il cognome all'utente
+
+const userSurname = prompt('Cognome');
+console.log(userSurname);
+
+// Chiedo il colore preferito
+
+const favoriteColor =prompt('colore preferito');
+console.log(favoriteColor);
+
+// Calcolo la password
+
+const sum = userName + userSurname + favoriteColor + 21
+console.log(sum);
+console.log(typeof sum);
+
+// inserirlo in pagina
+
+targetElement.innerText = sum;
